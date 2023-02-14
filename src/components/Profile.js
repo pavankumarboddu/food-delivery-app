cec6a6e1801152187ec1d747eb9ff62f5903451b
Pavan Kumar Boddu <writetopavanboddu@gmail.com>
2023-02-14 15:18:19 +0530

    Initial Commit

---
+++ src/components/Profile.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+const Profile = (props) => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>Porofile Component</h2>
+      <h3>Name: {props.name}</h3>
+      <h3>Count: {count}</h3>
+      <button
+        onClick={() => {
+          setCount(1);
+        }}
+      >
+        Count
+      </button>
+    </div>
+  );
+};
+
+export default Profile;
